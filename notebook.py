f85c075a487990929d756ba4cc25b2f1cee0cc19
Karn Wong <karn@karnwong.me>
2025-08-24 09:28:12 +0700

    add pandas & pydantic

--- notebook.py
+++ notebook.py
@@ -8,12 +8,62 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo  # noqa: F401
 
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Pandas Dataframe""")
     return
 
 
 @app.cell
 def _():
-    print("hello")
+    from string import ascii_uppercase as letters
+
+    import numpy as np
+    import pandas as pd
+
+    df = pd.DataFrame(
+        np.random.randint(0, 100, size=(100, len(letters))), columns=list(letters)
+    )
+    df
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# Display JSON""")
+    return
+
+
+@app.cell
+def _():
+    json_data = ["foo", {"bar": ("baz", None, 1.0, 2)}]
+    json_data
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# pydantic""")
+    return
+
+
+@app.cell
+def _():
+    from pydantic import BaseModel
+
+    class MeetingInfo(BaseModel):
+        attendees: int = 5
+        meeting_duration_minutes: int = 30
+        times_per_week: int = 1
+        avg_salary: int = 100000
+
+    meeting = MeetingInfo()
+    print(meeting)
+    print()
+    print(f"Meeting duration (m): {meeting.meeting_duration_minutes}")
     return
 
 
